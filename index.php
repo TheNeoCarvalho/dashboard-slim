81e797508b62f6393d95f1a569758ec416907504
Eduardo da Silva <eduardosilvaphp@gmail.com>
2019-11-29 13:58:51 -0300

    0.3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="pt-br">
     <head>
-        <title>dashboard</title>
+        <title>Dashboard</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -14,9 +14,9 @@
         <div class="row w-100" style="height: 100vh;">
 
             <div class="col h-100 col-lg-2 bg-dark"> 
-            <!-- barra lateral -->         
+            <!-- slide bar -->         
                 <center class="text-white" style="margin-top:20px; margin-bottom: 20px;">
-                    <svg id="i-activity" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="70" height="70" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                    <svg id="i-activity" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="100" height="100" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
                         <path d="M4 16 L11 16 14 29 18 3 21 16 28 16" />
                     </svg>
                 </center>
@@ -40,11 +40,19 @@
             </div>
 
             <div class="col">
-            <!-- barra do topo -->
-                <nav class="bg-light navbar navbar-expand-sm navbar-light border-bottom w-100">           
+            <!-- top bar -->
+                <nav class="bg-light navbar navbar-expand-sm navbar-light border-bottom w-100 shadow-sm rounded">           
                     <div class="collapse navbar-collapse" id="collapsibleNavId">
-                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <a href="#"class="text-monospace text-decoration-none text-body font-weight-light"><h4>Dashboard</h4></a>
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0" style="display: flex;align-items: center;justify-content: center">
+                            <svg id="i-search" class="h-100" style="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                <circle cx="14" cy="14" r="12" />
+                                <path d="M23 23 L30 30"  />
+                            </svg>
+                            <!-- Search form -->
+                            <form class="form-inline">
+                                <i class="fas fa-search" aria-hidden="true"></i>
+                                <input class="form-control form-control-sm ml-3 w-100" style="" type="text" placeholder="procurar"aria-label="Search">
+                            </form>
                         </ul>
                         <!-- Button trigger modal -->
                         <button type="button" class="btn" style="margin-right:5px;" data-toggle="modal" data-target="#modelId">
@@ -88,13 +96,57 @@
                             </button>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="triggerId">
                                 <a class="dropdown-item" href="#">perfil</a>
-                                <a class="dropdown-item" href="#">configuração</a>
+                                <a class="dropdown-item" href="#">configurações</a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#">sair</a>
                             </div>
                         </div>
                     </div>         
                 </nav> 
+                <!-- -->
+                <div class="row text-center mt-3">
+                    <div class="col bg-primary m-3 p-3 rounded shadow-sm">
+                        <div class="row text-right text-white align-middle">
+                            <div class="col-3" style="display: flex;align-items: center;justify-content: center">
+                                <svg id="i-mobile" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="49" height="49" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                    <path d="M21 2 L11 2 C10 2 9 3 9 4 L9 28 C9 29 10 30 11 30 L21 30 C22 30 23 29 23 28 L23 4 C23 3 22 2 21 2 Z M9 5 L23 5 M9 27 L23 27" />
+                                </svg>
+                            </div>
+                            <div class="col">
+                                <h4>223456623</h4>
+                                <h6 class="text-white-50">Usos do SRM hoje</h6>
+                            </div>
+                        </div>     
+                    </div>
+                    <div class="col bg-primary m-3 p-3 rounded shadow-sm">
+                        <div class="row text-right text-white align-middle">
+                            <div class="col-3" style="display: flex;align-items: center;justify-content: center">
+                                <svg id="i-download" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="49" height="49" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                    <path d="M9 22 C0 23 1 12 9 13 6 2 23 2 22 10 32 7 32 23 23 22 M11 26 L16 30 21 26 M16 16 L16 30" />
+                                </svg>
+                            </div>
+                            <div class="col">
+                                <h4>1321233</h4>
+                                <h6 class="text-white-50">Downloads</h6>
+                            </div>
+                        </div>     
+                    </div>
+                    <div class="col bg-primary  m-3 p-3 rounded shadow-sm" style="">
+                        <div class="row text-right text-white align-middle">
+                            <div class="col-3" style="display: flex;align-items: center;justify-content: center">
+                                <svg id="i-cart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="49" height="49" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                    <path d="M6 6 L30 6 27 19 9 19 M27 23 L10 23 5 2 2 2" />
+                                    <circle cx="25" cy="27" r="2" />
+                                    <circle cx="12" cy="27" r="2" />
+                                </svg>
+                            </div>
+                            <div class="col">
+                                <h4>R$ 23432390</h4>
+                                <h6 class="text-white-50">Lucro atual</h6>
+                            </div>
+                        </div>         
+                    </div>
+                </div>
             </div>    
         </div>
 
@@ -103,5 +155,6 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     </body>
 </html>
